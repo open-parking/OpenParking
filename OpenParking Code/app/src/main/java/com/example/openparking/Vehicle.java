53e32c2fbf9b68cd919ba2ed242d09ce66f0b378
Sepehr <sep.farahani@gmail.com>
2019-02-04 15:51:57 -0800

    add vehicle

---
+++ OpenParking Code/app/src/main/java/com/example/openparking/Vehicle.java
@@ -0,0 +1,37 @@
+package com.example.openparking;
+
+public class Vehicle {
+    String brand;
+    String model;
+    String year;
+    String color;
+    String licensePlate;
+
+    public Vehicle(String brand, String model, String year, String color, String licensePlate) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.licensePlate = licensePlate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+}
