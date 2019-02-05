dc8d26317a26857fa6c286539d9b688da40ea472
Barac <jaimeminpark@gmail.com>
2019-02-04 16:26:38 -0800

    Add toString to Vehicle

--- OpenParking Code/app/src/main/java/com/example/openparking/Vehicle.java
+++ OpenParking Code/app/src/main/java/com/example/openparking/Vehicle.java
@@ -35,26 +35,6 @@ public class Vehicle {
         return licensePlate;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setLicensePlate(String licensePlate) {
-        this.licensePlate = licensePlate;
-    }
-
     @Override
     public String toString() {
         return "Vehicle{" +
