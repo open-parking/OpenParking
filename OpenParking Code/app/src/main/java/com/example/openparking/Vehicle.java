a5405977e5453f645d0aba3446f9d80050e70d7d
Sepehr <sep.farahani@gmail.com>
2019-02-04 16:12:40 -0800

    Update Vehicle.java

--- OpenParking Code/app/src/main/java/com/example/openparking/Vehicle.java
+++ OpenParking Code/app/src/main/java/com/example/openparking/Vehicle.java
@@ -19,19 +19,39 @@ public class Vehicle {
         return brand;
     }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public String getModel() {
         return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 
     public String getYear() {
         return year;
     }
 
+    public void setYear(String year) {
+        this.year = year;
+    }
+
     public String getColor() {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public String getLicensePlate() {
         return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
     }
 }
