94f30bbd3053c794276df3feaf328c9c24cc9a1d
lopezp <lopezp9492@gmail.com>
2019-02-11 15:04:01 -0800

    Added User Location and Logo

--- OpenParking Code/app/src/main/java/com/example/openparking/MainActivity.java
+++ OpenParking Code/app/src/main/java/com/example/openparking/MainActivity.java
@@ -9,8 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+// Writing to file
+import java.io.*;
+import java.io.File;
+import android.os.Environment;
+import java.io.FileWriter;
+
+
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int ERROR_DIALOG_REQUEST = 9001;
 
-
+    Button btnMap;
+    Button btnFileWrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +41,31 @@ public class MainActivity extends AppCompatActivity {
         {
             init();
         }
+
+
     }
 
     private void init()
     {
-        Button btnMap  = (Button) findViewById(R.id.btnMap);
+        btnMap  =  findViewById(R.id.btnMap);
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.v(TAG, "Clicked Map Button");
+
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnFileWrite  = (Button) findViewById(R.id.btnFileWrite);
+        btnFileWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Clicked Write Button");
+
+                //Testing Writing to file
+                generateNoteOnSD( "Hello", "World");
             }
         });
     }
@@ -62,5 +88,22 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "You cant make map requests", Toast.LENGTH_SHORT).show();
         }
         return false;
+    }
+
+    public void generateNoteOnSD( String sFileName, String sBody) {
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+            Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
