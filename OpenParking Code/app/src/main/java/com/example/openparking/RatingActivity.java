2f7df3ff6826b20d4d11e390ade76ade6665d126
Sepehr <sep.farahani@gmail.com>
2019-02-27 15:50:31 -0800

    RatingActivity

---
+++ OpenParking Code/app/src/main/java/com/example/openparking/RatingActivity.java
@@ -0,0 +1,62 @@
+package com.example.openparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+
+        final RatingBar mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+        final TextView mRatingScale = (TextView) findViewById(R.id.tvRatingScale);
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        final TextView ratingDisplayTextView = (TextView) findViewById(R.id.rating_display_text_View);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ratingDisplayTextView.setText("You rating is: " + mRatingBar.getRating());
+            }
+        });
+
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+            }
+        });
+
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                mRatingScale.setText(String.valueOf(v));
+                switch ((int) ratingBar.getRating()) {
+                    case 1:
+                        mRatingScale.setText("Very bad");
+                        break;
+                    case 2:
+                        mRatingScale.setText("Need some improvement");
+                        break;
+                    case 3:
+                        mRatingScale.setText("Good");
+                        break;
+                    case 4:
+                        mRatingScale.setText("Great");
+                        break;
+                    case 5:
+                        mRatingScale.setText("Awesome. I love it");
+                        break;
+                    default:
+                        mRatingScale.setText("");
+                }
+            }
+        });
+    }
+}
