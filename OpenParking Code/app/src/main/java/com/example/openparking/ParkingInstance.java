62f35da8bd580a55bd2858b56546099ccae913ca
Unknown <jaimeminpark@gmail.com>
2019-03-11 02:51:50 -0700

    Add more data types to ParkingInstance

--- OpenParking Code/app/src/main/java/com/example/openparking/ParkingInstance.java
+++ OpenParking Code/app/src/main/java/com/example/openparking/ParkingInstance.java
@@ -13,6 +13,13 @@ public class ParkingInstance {
     private Address address;
     private LatLng latlng; //Possibly redundant, could be derived from address variable
 
+    private int price;
+    private String desc;
+    private Date availabilityStart; //Time when it is available
+    private Date availabilityEnd; //Time when availability ends
+    //TODO: scheduling throughout the week?
+    private String preferredPaymentMethod;
+
     private Date starttime; //Time when vehicle parked at the address;
     private Date endtime;   //Time when vehicle left the parking spot.
 
@@ -49,4 +56,99 @@ public class ParkingInstance {
         endtime = new  Date();
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public LatLng getLatlng() {
+        return latlng;
+    }
+
+    public void setLatlng(LatLng latlng) {
+        this.latlng = latlng;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getAvailabilityStart() {
+        return availabilityStart;
+    }
+
+    public void setAvailabilityStart(Date availabilityStart) {
+        this.availabilityStart = availabilityStart;
+    }
+
+    public Date getAvailabilityEnd() {
+        return availabilityEnd;
+    }
+
+    public void setAvailabilityEnd(Date availabilityEnd) {
+        this.availabilityEnd = availabilityEnd;
+    }
+
+    public String getPreferredPaymentMethod() {
+        return preferredPaymentMethod;
+    }
+
+    public void setPreferredPaymentMethod(String preferredPaymentMethod) {
+        this.preferredPaymentMethod = preferredPaymentMethod;
+    }
+
+    public Date getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(Date starttime) {
+        this.starttime = starttime;
+    }
+
+    public Date getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
 }
