d1ffea4bb3e407f0fb61da6dba5f5c46cbcc9838
Harvey <cjhanh@gmail.com>
2019-03-19 22:07:37 -0700

    profile
    Enabled user to add photos

--- OpenParking Code/app/src/main/java/com/example/openparking/SignupActivity.java
+++ OpenParking Code/app/src/main/java/com/example/openparking/SignupActivity.java
@@ -1,9 +1,14 @@
 package com.example.openparking;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,11 +16,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -27,13 +34,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.w3c.dom.Text;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonRegister;
-
+    static int PReqCode = 1 ;
+    static int REQUESCODE = 1 ;
     private EditText editTextEmail;
     private String email;
 
@@ -45,7 +56,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
     private TextView textViewSignin;
 
-
+    ImageView ImgUserPhoto;
+    Uri pickedImgUri;
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase mDatabase;
@@ -56,7 +68,21 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ImgUserPhoto = findViewById(R.id.imgUserPhoto);
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >= 22)
+                {
+                    checkAndRequestForPermission();
+                }
+                else
+                {
+                    openGallery();
+                }
+            }
+        });
 
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -84,7 +110,41 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         buttonRegister.setOnClickListener(this);
         textViewSignin.setOnClickListener(this);
     }
+    private void updateUserInfo(String name, Uri pickedImgUri, final FirebaseUser currentUser)
+    {
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("user_photos");
+        final StorageReference imageFilePath = mStorage.child(pickedImgUri.getLastPathSegment());
+        imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // image uploaded successful
 
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        //url containing user img
+
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setPhotoUri(uri)
+                                .build();
+                        currentUser.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                {
+                                    //user info update
+                                    showMessage("Register Complete");
+                                    updateUI();
+                                }
+                            }
+                        });
+
+                    }
+                });
+
+            }
+        });
+    }
 
     private void registerUser() {
         email = editTextEmail.getText().toString().trim();
@@ -193,7 +253,49 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                 });
         // [END send_email_verification]
     }
+    private void updateUI() {
 
+        Intent homeActivity = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(homeActivity);
+        finish();
+
+
+    }
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+
+    }
+    private void openGallery() {
+        //TODO: open gallery intent and wait for user to pick an image !
+
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent,REQUESCODE);
+    }
+    private void checkAndRequestForPermission() {
+
+
+        if (ContextCompat.checkSelfPermission(SignupActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(SignupActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                Toast.makeText(SignupActivity.this,"Please accept for required permission",Toast.LENGTH_SHORT).show();
+
+            }
+
+            else
+            {
+                ActivityCompat.requestPermissions(SignupActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PReqCode);
+            }
+
+        }
+        else
+            openGallery();
+
+    }
     @Override
     public void onClick(View view) {
         if(view == buttonRegister)
@@ -205,5 +307,21 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             //will open login activity here
             startActivity(new Intent(this, LoginActivity.class));
         }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUESCODE && data != null ) {
+
+            // the user has successfully picked an image
+            // we need to save its reference to a Uri variable
+            pickedImgUri = data.getData() ;
+            ImgUserPhoto.setImageURI(pickedImgUri);
+
+
+        }
+
+
     }
 }
