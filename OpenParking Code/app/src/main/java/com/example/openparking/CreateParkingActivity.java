4430f7e6afd8e68e01bc2494f2a26c42aade4d86
Harvey <cjhanh@gmail.com>
2019-04-07 14:57:41 -0700

    ui for adding parking instance

---
+++ OpenParking Code/app/src/main/java/com/example/openparking/CreateParkingActivity.java
@@ -0,0 +1,13 @@
+package com.example.openparking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateParkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_parking);
+    }
+}
