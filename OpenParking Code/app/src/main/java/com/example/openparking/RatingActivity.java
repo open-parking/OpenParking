d7bce6f75d37e9ed315d363d810c18cf5e89fd7e
Unknown <jaimeminpark@gmail.com>
2019-05-06 08:10:38 -0700

    WIP ratings firebase

--- OpenParking Code/app/src/main/java/com/example/openparking/RatingActivity.java
+++ OpenParking Code/app/src/main/java/com/example/openparking/RatingActivity.java
@@ -2,13 +2,20 @@ package com.example.openparking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -21,8 +28,11 @@ public class RatingActivity extends AppCompatActivity {
     private FirebaseDatabase mDatabase;
     private DatabaseReference usersRef;
 
-    public RatingActivity(FirebaseDatabase mDatabase) {
+    /*public RatingActivity(FirebaseDatabase mDatabase) {
         this.mDatabase = mDatabase;
+    }*/
+    public RatingActivity() {
+        usersRef = FirebaseDatabase.getInstance().getReference();
     }
 
     @Override
@@ -44,7 +54,7 @@ public class RatingActivity extends AppCompatActivity {
             finish();
             startActivity(new Intent(this, LoginActivity.class));
         }
-        FirebaseUser fuser = firebaseAuth.getCurrentUser();
+        final FirebaseUser fuser = firebaseAuth.getCurrentUser();
         final User user = new User();
         user.setId(fuser.getUid());//USER ID
 
@@ -52,16 +62,89 @@ public class RatingActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                mDatabase.child("users").child(userRatingTotal).addChildEventListener(
+                        new ChildEventListener() {
+
+                            @Override
+                            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                                Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+
+                                User mUser = new User();
+                                mUser.setUserRating(userRatingTotal);
+                                String uID = user.getId();
+                                mUser.setId(uID);
+                                mUser= dataSnapshot.getValue(User.class);
+
+                                    Log.d(TAG, "onChildAdded: " +  "ps is good");
+
+                                    parkingSpaceList.add(ps);
+
+                                    String address = ps.getAddress();
+                                    String hours = "Hours: " + ps.getOpentime() + " to " + ps.getClosetime();
+
+                                    MarkerOptions mo = new MarkerOptions();
+                                    mo.position(new LatLng(ps.getLatitude(), ps.getLongitude() ) );
+                                    mo.title(address);
+                                    mo.snippet(hours);
+                                    mo.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                                    mo.flat(true);
+
+                                    //Add marker to map AND get its ID
+                                    String markerID = mMap.addMarker(mo).getId();
+
+                                    parkingSpaceHashMap.put(markerID, ps);
+                            }
+
+                            @Override
+                            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                                Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+
+                                // A comment has changed, use the key to determine if we are displaying this
+                                // comment and if so displayed the changed comment.
+                                //Comment newComment = dataSnapshot.getValue(Comment.class);
+                                //String commentKey = dataSnapshot.getKey();
+
+                                // ...
+                            }
+
+                            @Override
+                            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                                Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+
+                                // A comment has changed, use the key to determine if we are displaying this
+                                // comment and if so remove it.
+                                // commentKey = dataSnapshot.getKey();
+
+                                // ...
+                            }
+
+                            @Override
+                            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                                Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+
+                                // A comment has changed position, use the key to determine if we are
+                                // displaying this comment and if so move it.
+                                //Comment movedComment = dataSnapshot.getValue(Comment.class);
+                                //String commentKey = dataSnapshot.getKey();
+
+                                // ...
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+                                Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+                                //Toast.makeText(mContext, "Failed to load comments.",
+                                //Toast.LENGTH_SHORT).show();
+                            }
+
+                        });
+
                 //ratingDisplayTextView.setText("Your rating is: " + mRatingBar.getRating());
                 userRatingTotal = user.getUserRating() * user.getTimesUserRated() + mRatingBar.getRating();
                 user.setTimesUserRated(user.getTimesUserRated() + 1);
                 user.setUserRating(userRatingTotal / user.getTimesUserRated());
                 ratingDisplayTextView.setText("Your average rating is: " + user.getUserRating());
-
-                User mUser = new User();
-                mUser.setUserRating(userRatingTotal);
-                String uID = user.getId();
-                mUser.setId(uID);
             }
         });
 
